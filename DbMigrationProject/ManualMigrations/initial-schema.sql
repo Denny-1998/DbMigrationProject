8a8be182a242fdceee3e35ed2cb7413416bd5621
Denny-1998 <89900734+Denny-1998@users.noreply.github.com>
2024-10-27 13:43:10 +0100

    added creation of DB to the script

--- DbMigrationProject/ManualMigrations/initial-schema.sql
+++ DbMigrationProject/ManualMigrations/initial-schema.sql
@@ -1,3 +1,12 @@
+IF DB_ID('DbMigrationProject01') IS NULL
+BEGIN
+    CREATE DATABASE DbMigrationProject01;
+END
+GO
+
+USE DbMigrationProject01;
+GO
+
 CREATE TABLE Products (
     Id INT PRIMARY KEY IDENTITY(1,1),
     Name VARCHAR(255) NOT NULL,
