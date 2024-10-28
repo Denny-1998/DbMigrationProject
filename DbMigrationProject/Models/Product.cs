574b9c6a2ec622d3ab4c3166a83e6d18a5df2e60
Denny-1998 <89900734+Denny-1998@users.noreply.github.com>
2024-10-28 22:51:10 +0100

    added model classes for category, product and product rating. added db context

---
+++ DbMigrationProject/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using DbMigrationProject.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; } // Foreign key to Category
+    public Category Category { get; set; }
+    public ICollection<ProductRating> Ratings { get; set; }
+}
---
+++ DbMigrationProject/ProjectDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DbMigrationProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ProjectDbContext : DbContext
+{
+    public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<ProductRating> ProductRatings { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
