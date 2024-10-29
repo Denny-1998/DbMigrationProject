b928dd31a08beb49126f40368b339f6cbdc33aa4
Denny-1998 <89900734+Denny-1998@users.noreply.github.com>
2024-10-29 14:58:39 +0100

    added configurations for dependency injection and ef.

--- DbMigrationProject/Program.cs
+++ DbMigrationProject/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using DbMigrationProject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+string json;
+DatabaseCredentials credentials;
+
+try
+{
+json = File.ReadAllText("db_credentials.json");
+credentials = JsonSerializer.Deserialize<DatabaseCredentials>(json);
+} 
+catch (Exception ex)
+{
+    throw new Exception("db_credentials.json file not found.\n\n\n\n"+ex);
+}
+
+
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        var connectionString = $"Server=localhost;Database=DbMigrationProject01;User Id={credentials.Username};Password={credentials.Password};";
+
+        services.AddDbContext<ProjectDbContext>(options =>
+            options.UseSqlServer(connectionString));
+    })
+    .Build();
+
+Console.WriteLine("Dependency injection configured.");
+
+host.Run();
